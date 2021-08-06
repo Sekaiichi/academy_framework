f7c5b500c7c5d057badffe5a2b99967aed7c11b7
mehrubon <husaynov888@gmail.com>
2021-08-06 19:43:00 +0500

    Refactoring

--- src/Framework/Http/Router/RouteCollection.php
+++ src/Framework/Http/Router/RouteCollection.php
@@ -4,7 +4,8 @@
 namespace Framework\Http\Router;
 
 
-namespace Framework\Http\Router;
+use Framework\Http\Router\Route\RegexRoute;
+use Framework\Http\Router\Route\Route;
 
 class RouteCollection
 {
@@ -17,26 +18,26 @@ class RouteCollection
 
     public function add($name, $pattern, $handler, array $methods, array $tokens = []): void
     {
-        $this->addRoute(new Route($name, $pattern, $handler, $methods, $tokens));
+        $this->addRoute(new RegexRoute($name, $pattern, $handler, $methods, $tokens));
     }
 
     public function any($name, $pattern, $handler, array $tokens = []): void
     {
-        $this->addRoute(new Route($name, $pattern, $handler, [], $tokens));
+        $this->addRoute(new RegexRoute($name, $pattern, $handler, [], $tokens));
     }
 
     public function get($name, $pattern, $handler, array $tokens = []): void
     {
-        $this->addRoute(new Route($name, $pattern, $handler, ['GET'], $tokens));
+        $this->addRoute(new RegexRoute($name, $pattern, $handler, ['GET'], $tokens));
     }
 
     public function post($name, $pattern, $handler, array $tokens = []): void
     {
-        $this->addRoute(new Route($name, $pattern, $handler, ['POST'], $tokens));
+        $this->addRoute(new RegexRoute($name, $pattern, $handler, ['POST'], $tokens));
     }
 
     /**
-     * @return Route[]
+     * @return RegexRoute[]
      */
     public function getRoutes(): array
     {
